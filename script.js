736e5d9609fffc055d547760f887da04c85c8477
Hussayn <94265187+Husayn01@users.noreply.github.com>
2022-11-05 10:05:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function isLeapYear(){
+    var year = document.getElementById("demo").value;
+    if (year % 4 == 0 ) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                document.getElementById("result").innerHTML = year + " is a leap year";
+            } else {
+                document.getElementById("result").innerHTML = year + " is not a leap year";
+            }
+        } else {
+            document.getElementById("result").innerHTML = year + " is a leap year";
+        }
+    } else {
+        document.getElementById("result").innerHTML = year + " is not a leap year";
+    }
+}
